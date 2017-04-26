63b3751ef78ffea8d6cbd4dda11216e30401ff1e
Dominic Brown and Monique St Laurent <dominic+monique@>
2017-04-26 14:09:16 -0700

    add code to pass all specs

--- app.rb
+++ app.rb
@@ -1,36 +1,52 @@
-require 'sinatra'
-require 'sinatra/reloader'
-require './lib/dealership'
-require './lib/vehicle'
-require 'pry'
-
+require('sinatra')
+require('sinatra/reloader')
 also_reload('lib/**/*.rb')
+require('./lib/vehicle')
+require('./lib/dealership')
+require('pry')
 
 get('/') do
   erb(:index)
 end
 
-get('/vehicles') do
-  @vehicles = Vehicle.all()
-  @vehicles_list = Vehicle.all().length()
-  erb(:vehicles)
+get('/dealership/new') do
+   erb(:dealerships_form)
 end
 
-get('/vehicles/new') do
-  erb(:vehicles_form)
+get('/dealerships') do
+  @dealerships = Dealership.all()
+  erb(:dealerships)
 end
 
-post('/vehicles_input') do
-  make = params.fetch("make")
-  model = params.fetch("model")
-  year = params.fetch("year")
-  vehicle = Vehicle.new(make, model, year)
-  vehicle.save()
-  @vehicles_list = Vehicle.all().length()
+post('/dealerships') do
+  name = params.fetch('name')
+  Dealership.new(name).save()
+  @dealerships = Dealership.all()
   erb(:success)
 end
 
+get('/dealerships/:id') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:dealership)
+end
+
+get('/dealerships/:id/vehicles/new') do
+    @dealership = Dealership.find(params.fetch('id').to_i())
+    erb(:dealership_vehicles_form)
+end
+
 get('/vehicles/:id') do
-  @vehicle = Vehicle.find(params.fetch("id"))
+  @vehicle = Vehicle.find(params.fetch('id').to_i())
   erb(:vehicle)
+end
+
+post('/vehicles') do
+  make = params.fetch('make')
+  model = params.fetch('model')
+  year = params.fetch('year')
+  @vehicle = Vehicle.new(make, model, year)
+  @vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
+  erb(:success)
 end
